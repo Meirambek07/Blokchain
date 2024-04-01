06d44050abba53603a7b09d383748b49c503817c
Meirambek07 <164979023+Meirambek07@users.noreply.github.com>
2024-04-01 15:45:11 +0600

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 token = "6815863446:AAEAflG9KKRvpUVvSnotWDZz5bpW6A-KhMw"
 
 rpc_user = "kzcashrpc"
-rpc_password = "gURkU86vWzmAOLbo3paaPRcd"
+rpc_password = "ddg23ZnPSXgKNJOLVb7XBYza"
 rpc_host = "127.0.0.1"
 rpc_port = 8277
